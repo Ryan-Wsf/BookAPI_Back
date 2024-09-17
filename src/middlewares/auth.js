36677de2200f139496156511c6723dc8872eb00a
unknown <wassouf.ryan@outlook.fr>
2024-09-17 11:43:38 +0200

    update: register/login

--- src/middlewares/auth.js
+++ src/middlewares/auth.js
@@ -4,7 +4,8 @@ require ('dotenv').config();
 
 const authMiddleware = async (req, res, next) => {
     try{
-        const token = req.headers.autorization.split(' ')[1];
+        console.log(req.headers);
+        const token = req.headers.authorization.split(' ')[1];
 
         if(!token){
             return res.status(401).json({message: 'Authentication failed'});
